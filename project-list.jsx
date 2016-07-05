b7fcd7fd879594a885e492be1423ebab22e0b75f
padulam <padula.matthew@gmail.com>
2016-07-05 17:44:40 -0400

    Add react component for projects. Remove background image.

---
+++ project-list.jsx
@@ -0,0 +1,68 @@
+class Project extends React.Component {
+  render(){
+    return(
+	<li className="pic-item">
+		<a href= {this.props.url} target="_blank">
+			<img className="project-screenshots" src={this.props.image} alt={`Screenshot of ${this.props.name}`}/>
+			<div className="caption">
+				<div className="blur"></div>
+				<div className="caption-text">
+				<h1>{this.props.name}</h1>
+				<p>Matthew Padula</p>
+				</div>
+			</div>
+		</a>
+	</li>
+    )
+  }
+}
+
+class ProjectList extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {projects:[]};
+	}
+
+	componentDidMount(){
+		this._getProjects("projects.json");
+	}
+
+	_getProjects(projectUrl){
+		$.ajax({
+			method: 'GET',
+			url: projectUrl,
+			success: (projects)=>{
+				this.setState({projects: JSON.parse(projects)})
+			}
+		})
+	}
+
+	_getProjectList(){
+		return this.state.projects.map((project)=>{
+			return(
+				<Project 
+					id={project.id} 
+					key={project.id} 
+					name={project.name}
+					url={project.url}
+					image={project.screenshot}
+				/>
+			)
+		});
+	}
+
+	render(){
+		const projects = this._getProjectList();
+
+		return(
+			<ul className="caption-style-1">
+				{projects}
+			</ul>
+		)
+  	}
+}
+
+ReactDOM.render(
+	<ProjectList />, document.getElementById('projectContainer')
+)
