363b0026d6a7e0e591b45fe1e8e4afc8e55dc85c
padulam <padula.matthew@gmail.com>
2016-07-10 22:20:45 -0400

    Update weather api to forecast.io.

--- projects/weather-app/index.js
+++ projects/weather-app/index.js
@@ -1,86 +1,95 @@
 var temperature;
 
 $(document).ready(function() {
-  var zip;
-  var getZip = $.get("https://freegeoip.net/json/", function(response) {
-    zip = response.zip_code;
-    $('#location').html(response.city + " , " + response.region_name);
+  var latlong;
+  var getZip = $.get("https://freegeoip.io/json/", function(response) {
+  latlong = response.latitude + ", " + response.longitude;
+  $('#location').html(response.city + " , " + response.region_name);
   }, "jsonp");
 
-  $.when(getZip).done(function() {
-    $.get("//api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&APPID=5cb4268e79277a890113e3dff474cba8", function(response) {
-      var backgroundURL;
-      var weatherBackground = {
-        snow: '../../images/cold-snow-landscape-nature.jpg',
-        rain: '../../images/red-glass-rainy-rain.jpg',
-        sunny: '../../images/sunny-beach-summer-path.jpg',
-        cloudy: '../../images/sea-nature-beach-clouds.jpg',
-        night: '../../images/sky-clouds-moon-horizon.jpg',
-        thunderstorm: '../../images/city-lights-night-clouds.jpg'
-      };
-
-      var d = new Date();
-      var weatherIcon = "//openweathermap.org/img/w/" + response.weather[0].icon + ".png";
-      temperature = response.main.temp;
-
-      if (d.getHours() >= 18) {
-        backgroundURL = weatherBackground['night'];
-      } else {
-
-        switch (response.weather[0].main) {
-          case "Clear":
-            backgroundURL = weatherBackground['sunny'];
-            break;
-            
-          case "Clouds":
-            if (response.weather[0].description === "clear sky") {
-              backgroundURL = weatherBackground['sunny'];
-            } else {
-              backgroundURL = weatherBackground['cloudy'];
-            }
-            break;
-
-          case "Snow":
-            backgroundURL = weatherBackground['snow'];
-            break;
-
-          case "Drizzle":
-          case "Rain":
-            backgroundURL = weatherBackground['rain'];
-            break;
-
-          case "Thunderstorm":
-            backgroundURL = weatherBackground['thunderstorm'];
-            break;
-        }
-      }
-      $('#mainScreen').css('background-image', 'url(' + backgroundURL + ')')
-      $('#temp').html(kelToFar(response.main.temp) + '&#8457');
-      $('#weatherPic').attr('src', weatherIcon);
-      $('#weatherDescription').html(response.weather[0].description);
-    });
+  $.ajax({
+    url: 'https://freegeoip.net/json/',
+    type: 'GET',
+    dataType: 'jsonp',
+    success: function(response){
+      getWeather(response.latitude + ", " + response.longitude, 
+          response.city, response.region_name);
+    }
   });
 });
+
+function getWeather(longlat,city,state){
+  $.ajax({
+    url: 'https://api.forecast.io/forecast/'+ //YOUR API KEY* 
+      +'/' + longlat,
+    type: 'GET',
+    dataType: 'jsonp',
+    success: function(response){
+      populateWeather(response);
+    }
+  });
+}
+
+function populateWeather(data){
+  var backgroundURL;
+  var weatherBackground = {
+    snow: '../../images/cold-snow-landscape-nature.jpg',
+    rain: '../../images/red-glass-rainy-rain.jpg',
+    sunny: '../../images/sunny-beach-summer-path.jpg',
+    cloudy: '../../images/sea-nature-beach-clouds.jpg',
+    night: '../../images/sky-clouds-moon-horizon.jpg',
+    thunderstorm: '../../images/city-lights-night-clouds.jpg'
+  };
+
+  temperature = data.currently.temperature;
+
+  switch (data.currently.icon) {
+    case "clear-day":
+      backgroundURL = weatherBackground['sunny'];
+      break;
+      
+    case "partly-cloudy-night":
+    case "clear-night":
+      backgroundURL = weatherBackground['night'];
+      break;
+
+    case "partly-cloudy-day":
+      backgroundURL = weatherBackground['cloudy'];
+      break;
+
+    case "snow":
+      backgroundURL = weatherBackground['snow'];
+      break;
+
+    case "Rain":
+      backgroundURL = weatherBackground['rain'];
+      break;
+
+    case "thunderstorm":
+      backgroundURL = weatherBackground['thunderstorm'];
+      break;
+  }
+
+  $('#mainScreen').css('background-image', 'url(' + backgroundURL + ')')
+  $('#temp').html(Math.round(temperature) + '&#8457');
+  $('#weatherPic').addClass("wi-forecast-io-" + data.currently.icon);
+  $('#weatherDescription').html(data.currently.summary);
+};
+
+function celToFar(temp) {
+  return Math.round((temp * 1.8000 + 32.00));
+};
 
 $('#unitOfDegrees').click(function() {
   if ($(this).hasClass('fahrenheit')) {
     $(this).html('Switch to Fahrenheit');
     $(this).removeClass('fahrenheit');
     $(this).addClass('celsius')
-    $('#temp').html(kelToCel(temperature) + '&#8451');
+    $('#temp').html(farToCel(temperature) + '&#8451');
   } else {
     $(this).html('Switch to Celsius');
     $(this).removeClass('celsius');
     $(this).addClass('fahrenheit');
-    $('#temp').html(kelToFar(temperature) + '&#8457');
+    $('#temp').html(Math.round(temperature) + '&#8457');
   }
-
 });
-
-function kelToFar(temp) {
-  return Math.round((temp - 273.15) * 1.8000 + 32.00)
-}
-
-function kelToCel(temp) {
-  return Math.round(temp - 273.15);
-}
