9b4992a12670ec2e20a8cd30b57eb33e2929a9cd
padulam <padula.matthew@gmail.com>
2016-07-22 21:53:15 -0400

    Add determine life method.

--- projects/game-of-life/gameoflife.jsx
+++ projects/game-of-life/gameoflife.jsx
@@ -14,7 +14,6 @@ class LifeApp extends React.Component {
     this._stopGame = this._stopGame.bind(this);
     this._clearGame = this._clearGame.bind(this);
     this._generateSquares = this._generateSquares.bind(this);
-    this._clearSquares = this._clearSquares.bind(this);
   }
 
   _generateSquares(){
@@ -27,23 +26,77 @@ class LifeApp extends React.Component {
         x=0;
         y+=40;
       }
-  		arr.push(<Square pos={[x,y]} id={i} key={i}/>);
+
+      let brdSqs = this._findBrdSqs([x,y]);
+
+  		arr.push(<Square highlightSquare={this._highlightSquare.bind(this)} brdSqs={brdSqs} pos={[x,y]} id={i} key={i}/>);
       x+=1;
   	}
 
   	this.setState({cells: arr});
   }
 
-  _findSquare(){
-    
+  _highlightSquare(arr){
+    let hSqs = [];
+    for(let i = 0; i<arr.length;i++){
+      this.state.cells.forEach(function(s){
+        if(arr[i][0]===s.props.pos[0]&&arr[i][1]===s.props.pos[1]){
+          hSqs.push(s);
+        }
+      });
+    }
   }
 
-  _clearSquares(){
-  	this.state.cells.map(e => {
-  		if(e.state.living){
-  			e.setState({living: false});
-  		}
-  	});
+  _determineLife(living){
+    let liveCnt = 0;
+
+    this.borderSqs.forEach(function(s){
+      if(s.props.living){
+        liveCnt+=1;
+      }
+    })
+
+    if(living&&liveCnt<2){
+      return false;
+    }else if(living&&(liveCnt===2||liveCnt===3)){
+      return true;
+    } else if(living&&liveCnt>3){
+      return false;
+    } else if(living===false&&liveCnt===3){
+      return true;
+    }
+  }
+
+  _findBrdSqs(arr){
+    const max_X = 39
+    const max_Y = 960
+    const min_X = 0
+    const min_Y = 0
+
+    let brdSqs = [[1,0],[-1,0],[0,40],
+          [0,-40],[1,40],[-1,-40],
+          [1,-40],[-1,40]];
+
+    let squares = [];
+
+    for(let i=0;i<brdSqs.length;i++){
+      let x = arr[0] + brdSqs[i][0];
+      let y = arr[1] + brdSqs[i][1];
+      if(x<min_X){
+        x= max_X;
+      } else if(x>max_X){
+        x= min_X
+      }
+
+      if(y<min_Y){
+        y= max_Y;
+      } else if(y>max_Y){
+        y= min_Y
+      }
+      squares.push([x,y]);
+    }
+
+    return squares;
   }
 
   _startGame(){
@@ -57,7 +110,7 @@ class LifeApp extends React.Component {
 
   _clearGame(){
   	this._stopGame();
-  	this._clearSquares();
+  	this._generateSquares();
   	this.setState({generations: 0});
   }
 
@@ -129,41 +182,9 @@ class Square extends React.Component {
 	}
 
 	_handleClick(){
-		this.setState({living: !this.state.living})
-    console.table(this._getSquare(this.props.pos));
+		this.setState({living: !this.state.living});
+    this.props.highlightSquare(this.props.brdSqs);
 	}
-
-  _getSquare(arr){
-    const max_X = 39
-    const max_Y = 960
-    const min_X = 0
-    const min_Y = 0
-
-    let sq = [[1,0],[-1,0],[0,40],
-          [0,-40],[1,40],[-1,-40],
-          [1,-40],[-1,40]];
-
-    let square = [];
-
-    for(let i=0;i<sq.length;i++){
-      let x = arr[0] + sq[i][0];
-      let y = arr[1] + sq[i][1];
-      if(x<min_X){
-        x= max_X;
-      } else if(x>max_X){
-        x= min_X
-      }
-
-      if(y<min_Y){
-        y= max_Y;
-      } else if(y>max_Y){
-        y= min_Y
-      }
-      square.push([x,y]);
-    }
-
-    return square;
-  }
 
 	render(){
 		let lifeClass ="";
